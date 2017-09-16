e8535ddee39fe827af30b24c247f44cf1dceb07b
adelavegaf <antoniodlavga@gmail.com>
2017-09-16 17:28:19 -0500

    Add mobile-first simplified project view.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,43 @@
+const MOCK_RESPONSES = [
+    {
+        name: "Fun",
+        id: 1,
+        text_description: "Fun game, fun time",
+        about_us: "Indie devs, no HQ.",
+        video_url: "",
+        image_url: "http://via.placeholder.com/350x150",
+        contact: "indiedev@gmail.com",
+        tags: ["software", "videogames", "adventure"]
+    },
+    {
+        name: "Tallest Skyscraper",
+        id: 2,
+        text_description: "Building a huge skyscraper",
+        about_us: "Montreal based construction team. 3rd year students.",
+        video_url: "vidurl",
+        image_url: "http://via.placeholder.com/350x150",
+        contact: "normal@yahoo.com",
+        tags: ["construction", "structures"]
+    },
+    {
+        name: "Yet Another Marketplace",
+        id: 3,
+        text_description: "proyect description. this may be really long",
+        about_us: "We are a fun, worldwide team!",
+        video_url: "vidurl",
+        image_url: "http://via.placeholder.com/350x150",
+        contact: "verylongemailusername@gmail.com",
+        tags: ["software", "marketplace"]
+    }
+];
+
 class Api {
     static getProjects() {
-        return Promise.resolve([{}, {}, {}]);
+        return Promise.resolve(MOCK_RESPONSES);
     }
+
     static getProject(id) {
-        return Promise.resolve({});
+        return Promise.resolve(MOCK_RESPONSES[2]);
     }
 }
 
--- frontend/src/components/ProjectList.js
+++ frontend/src/components/ProjectList.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
+import './ProjectList.css';
+
+const TAG_TYPES = ['tag badge badge-danger', 'tag badge badge-info', 'tag badge badge-success'];
 
 class ProjectList extends Component {
-    constructor(props) {
-        super(props);
+
+    generateProjectTags(tags) {
+        const tagsElementArray = [];
+        for (let i = 0; i < TAG_TYPES.length && i < tags.length; i++) {
+            tagsElementArray.push(
+                <span className={TAG_TYPES[i]} key={i}>{tags[i]}</span>
+            )
+        }
+        return tagsElementArray;
+    }
+
+    generateProjectList() {
+        return this.props.projects.map(project => {
+            return (
+                <div className="card project-holder col-xs-12" key={project.id}>
+                    <img className="card-img-top" src={project.image_url} alt="Card cap"/>
+                    <div className="card-body">
+                        <h4 className="card-title">{project.name}</h4>
+                        <p className="card-text">{project.text_description}</p>
+                        <hr/>
+                        <div className="text-center">
+                            {this.generateProjectTags(project.tags)}
+                        </div>
+                    </div>
+                </div>
+            );
+        });
     }
 
     render() {
         return (
             <div>
-                <button onClick={this.props.onProjectClick}>Boom</button>
+                {this.generateProjectList()}
             </div>
         )
     }
