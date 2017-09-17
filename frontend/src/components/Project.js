79b8cf748e7e5c2e3df4ad38f237a1ef84d621b1
Juan Sebastian Arciniegas <js.arciniegas10@uniandes.edu.co>
2017-09-17 13:28:31 -0500

    Contact Form UI

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -20,14 +20,32 @@ class Project extends Component
     createProjectView() {
             return (
                 <div className="row justify-content-center" key={this.props.project.id}>
-                    <div className="card project-holder col-12 col-md-8 col-lg-6">
-                        <img className="card-img-top" src={this.props.project.video_url} alt="Card cap"/>
-                        <div className="card-body">
-                            <h4 className="card-title">{this.props.project.name}</h4>
-                            <p className="card-text text-secondary">{this.props.project.long_description}</p>
-                        </div>
+                <div className="card project-holder col-12 col-md-8 col-lg-6">
+                    <div className="card-header text-center">{this.props.project.name}</div>
+                    <img className="card-img-top" src={this.props.project.video_url} alt="Card cap"/>
+                    <div className="card-body">
+                        <p className="card-text text-secondary">{this.props.project.long_description}</p>
+                    </div>
+                    <div className="card-footer">
+                    <div className="card-header text-center">Contact Form</div>
+                    
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <input className="form-control col-md-3" placeholder="Your Name" name="name"  type="text" />
+                            </div>
+                            <div className="form-group">
+                                <input className="form-control col-md-3" placeholder="Your Email" name="name"  type="text" />
+                            </div>
+                            <div className="form-group">
+                                <textarea className="form-control" placeholder="Your Message" name="comment" rows="3" type="text" ></textarea>
+                            </div>
+                            <div className="form-group text-center">
+                                <input className="btn btn-primary btn-lg" type="submit" value="Send Message"/>
+                            </div>
+                        </form>      
                     </div>
                 </div>
+            </div>
             );
         };
     }
