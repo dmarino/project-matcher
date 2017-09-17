dbf3d7d9132417da6e0f947286e18ccb35d047d9
Juan Sebastian Arciniegas <js.arciniegas10@uniandes.edu.co>
2017-09-17 13:55:44 -0500

    Ajustes UI

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -21,13 +21,14 @@ class Project extends Component
             return (
                 <div className="row justify-content-center" key={this.props.project.id}>
                 <div className="card project-holder col-12 col-md-8 col-lg-6">
-                    <div className="card-header text-center">{this.props.project.name}</div>
+                    <div className="card-header text-center"> <strong>{this.props.project.name}</strong></div>
                     <img className="card-img-top" src={this.props.project.video_url} alt="Card cap"/>
                     <div className="card-body">
                         <p className="card-text text-secondary">{this.props.project.long_description}</p>
                     </div>
                     <div className="card-footer">
-                    <div className="card-subheader text-center">Contact Form</div>
+                    <div className="card-subheader text-center"> <strong>Contact Form</strong></div>
+                    <label> <strong>{this.props.project.about_us}</strong></label>
                     <br/>
                     <div className="card-subbody">
                         <form onSubmit={this.handleSubmit}>
